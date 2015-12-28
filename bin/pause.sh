f0c9b34eb8c4978dd67eaa6100380ff8d9903c4e
Andrew Z Allen <me@andrewzallen.com>
2015-12-28 12:03:31 -0700

    Modify pause script to send to any chrome, not just google music.

--- bin/pause.sh
+++ bin/pause.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Pause music script in Awesome Window Manager
 
-WID=`xdotool search --name "Google Play Music" | head -1`
+# Preserve the desktop that was active so we can restore it
 DESKTOP=`xdotool get_desktop`
-xdotool set_desktop 8
-xdotool sleep .01 key --clearmodifiers --window $WID "space" sleep .01
+# Find the first instance of Google Chrome running on desktop 8
+WID=`xdotool search --desktop 8 --name "Google Chrome" | head -1`
+# For debugging print the window ID that is going to get the message
+echo "Setting $WID"
+# Use xdo tool to select the correct window, send it space (to pause)
+xdotool sleep .01 windowactivate --sync $WID sleep .01 key --clearmodifiers --window $WID "space" sleep .01
+# Now return focus to where it was
 xdotool set_desktop $DESKTOP
